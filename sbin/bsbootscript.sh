8c8d933153f490b98724d57ab7b4cae40ed80e6e
scott <scott@ubuntu>
2014-01-02 22:47:21 -0500

    fixup again
    Signed-off-by: scott <scott@ubuntu>

--- sbin/bsbootscript.sh
+++ sbin/bsbootscript.sh
@@ -1,10 +1,10 @@
-#!/sbin/sh
-#
+#!/system/bin/sh
 
-#remove the binaries as they are no longer needed. (kernel handled)
-if [ -e /system/bin/mpdecision ] ; then
-	busybox mv /system/bin/mpdecision /system/bin/mpdecision_bck
-fi
-if [ -e /system/bin/thermal-engine-hh ] ; then
-	busybox mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh_bck
-fi
+# disable sysctl.conf to prevent ROM interference with tunables
+# backup and replace PowerHAL with custom build to allow OC/UC to survive screen off
+# create and set permissions for /system/etc/init.d if it doesn't already exist
+mount -o rw,remount /system;
+[ -e /system/etc/sysctl.conf ] && mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
+[ -f /system/bin/mpdecision_bck ] || mv /system/bin/mpdecision /system/bin/mpdecision_bck
+[ -f /system/bin/thermal-engine-hh_bck ] || mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh_bck
+mount -o ro,remount /system;

40e5122317bf3e20316a2bc181d40c76779ea2d5
scott <scott@ubuntu>
2014-01-21 02:37:21 -0500

    Revert "switch to new linaro"
    This reverts commit cd8a1b175b7b44929d00eac5adb5a920eed9e992.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ BOARD_CUSTOM_BOOTIMG_MK := device/lge/hammerhead/mkbootimg.mk
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
 
 # The list below is order dependent
 BOARD_SEPOLICY_UNION := \

1cb5422e14776ab5eee824db1a8d09ca4a62937c
scott <scott@beanstalk.com>
2015-02-08 21:20:10 -0500

    Revert "hammerhead: Use halfres boot animation"
    This reverts commit eef5c9dfba244869d056756d2bc3e8d56c510922.

--- cm.mk
+++ cm.mk
@@ -1,7 +1,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common BS stuff.
 $(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
